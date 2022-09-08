4488c2ecfdd01ebfe383a68e1981ba87c1fef434
Shumskiy Evgeniy <vkzheny@gmail.com>
2022-09-08 21:13:23 +0300

    init

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,19 @@
+local cmp_status_ok, colorscheme = pcall(require, "ayu")
+if not cmp_status_ok then
+  return
+end
+-- vim.cmd [[
+-- try
+--   colorscheme ayu
+-- catch /^Vim\%((\a\+)\)\=:E185/
+--   colorscheme default
+--   set background=dark
+-- endtry
+-- ]]
+colorscheme.colorscheme()
+
+colorscheme.setup({
+    overrides = {
+        default = { bg = "#0f1419" },
+    }
+})
